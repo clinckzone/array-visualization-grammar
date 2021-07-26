e9c8484d1837f1f11ba2c1ebd81616ac80ef0d38
Divyanshu Tiwari <dtiwari@ar.iitr.ac.in>
2021-07-26 11:58:59 +0530

    Add and build Monaco editor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 //@ts-check
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import * as d3 from "d3";
-import { ArrayDiagram } from "./Diagrams/ArrayDiagram";
-import { push } from "./Functions/ArrayFunctions/Push";
-import { pop } from "./Functions/ArrayFunctions/Pop";
 
-const data = [10, 22, 31, 14, 25];
-const properties = {x: 10, y: 10, label: "Input Array"};
-let arrayDiagram = new ArrayDiagram(data, properties);
-
-d3.select("#push").on("click", push.bind(arrayDiagram));
-d3.select("#pop").on("click", pop.bind(arrayDiagram));
-d3.select("#search").on("click", arrayDiagram.search.bind(arrayDiagram));
+const runButton = document.getElementById("run-button");
+runButton.addEventListener('click', execute);
 
 const jsEditor = monaco.editor.create(document.getElementById('js-editor-container'), {
 	value: `function x() {\n\tconsole.log("Hello world!");\n}\n`,
@@ -19,6 +10,18 @@ const jsEditor = monaco.editor.create(document.getElementById('js-editor-contain
 });
 
 const jsonEditor = monaco.editor.create(document.getElementById('json-editor-container'), {
-	value: `function x() {\n\tconsole.log("Hello world!");\n}\n`,
-	language: 'javascript'
+	value: 
+`{
+	"calle": "push",
+	"data": [
+		{"name": "self", "type": "array", "value": [1, 2, 3, 4]},
+		{"name": "item", "type": "number", "value": 24},
+	]
+}`,
+	language: 'json'
 });
+
+function execute() {
+	const jsonData = jsonEditor.getValue();
+	console.log(jsonData);
+}
